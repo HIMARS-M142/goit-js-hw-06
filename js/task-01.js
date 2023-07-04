06eb3faf3106c5db324184e9387affa40dc009e8
HIMARS-M142 <maksympona@gmail.com>
2023-07-04 20:00:08 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,6 @@
 const allItemEl = document.querySelectorAll("ul#categories li.item");
 const numberLengthEl = `Number of categories: ${allItemEl.length}`;
-console.log(numberLengthEl);
+
 
 allItemEl.forEach((category) => {
   const titleEL = category.querySelector("h2").textContent;
